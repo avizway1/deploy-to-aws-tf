dc3e27338cbbaf138a7a3c1c0c17d39340d48d07
Avinash Reddy Thipparthi <57447641+avizway1@users.noreply.github.com>
2026-07-31 08:14:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,16 +4,16 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "aviz.tf.state.myproject"
-    key = "cicd/terraform.tfstate"
-    region = "ap-south-1"
+    bucket       = "aviz.tf.state.myproject"
+    key          = "cicd/terraform.tfstate"
+    region       = "ap-south-1"
     use_lockfile = true
   }
 }
 
 resource "aws_instance" "webserver" {
-  count = 1
-  ami  = "ami-00d2dbb426772b03a"
+  count         = 1
+  ami           = "ami-00d2dbb426772b03a"
   instance_type = "t3.micro"
 
   tags = {
